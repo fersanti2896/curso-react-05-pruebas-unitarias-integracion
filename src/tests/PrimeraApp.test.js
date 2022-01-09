3d6648d1fae3270c3c32ae45f8b70aa8da0ccaf0
Fernando Nicolas <fersanti2896@gmail.com>
2022-01-09 01:51:33 -0600

    Agregando enzyme y test, haciendo snapshot al componente

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import {render} from '@testing-library/react'
+import { shallow } from 'enzyme'
 import '@testing-library/jest-dom'
 import PrimeraApp from '../PrimeraApp'
 
-describe('Pruebas en Componenten PrimeraApp', () => {
-    test('Debe mostrar "Hola, soy Fernando"', () => {
-        const saludo = 'Hola, soy Fernando';
-        const { getByText } = render(<PrimeraApp saludo = {saludo}/>);
 
-        expect(getByText(saludo)).toBeInTheDocument();
+describe('Pruebas en Componenten PrimeraApp', () => {
+    /* Prueba con jest-dom */
+    // test('Debe mostrar "Hola, soy Fernando"', () => {
+    //     const saludo = 'Hola, soy Fernando';
+    //     const { getByText } = render(<PrimeraApp saludo = {saludo}/>);
+
+    //     expect(getByText(saludo)).toBeInTheDocument();
+    // });
+
+    /* Pruebas con Enzyme*/
+    test('Debe mostrar <PrimeraApp /> correctamente', () => {
+        const saludo = 'Hola, soy Fernando';
+        const wrapper = shallow(<PrimeraApp saludo = {saludo}/>);
+
+        expect(wrapper).toMatchSnapshot();
     })
-    
-})
+        
+});
